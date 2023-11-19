e4fc8324f15e5ebc7b4f3a676a7bda9e475b04f1
Artsem Kanapelka <artsem_kanapelka@epam.com>
2023-11-19 23:01:08 +0100

    feat: add getProductsList lambda

--- product-service/cdk.ts
+++ product-service/cdk.ts
@@ -1,1 +1,63 @@
-// TODO
+import * as cdk from 'aws-cdk-lib';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+// import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+// import { HttpApi, HttpMethod, CorsHttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+// import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+
+const app = new cdk.App();
+
+const stack = new cdk.Stack(app, 'CdkRsAwsShopBE', {
+    env: { region: 'eu-west-1' },
+});
+
+// const getProductsListLambdaFunction = new cdk.aws_lambda_nodejs.NodejsFunction(stack, 'get-products-list-lambda', {
+//     runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+//     functionName: 'getProductsList',
+//     entry: './handlers/getProductsList/index.ts',
+//     handler: 'getProductsList',
+//     // environment: { PRODUCT_AWS_REGION: 'eu-west-1' },
+//     bundling: {
+//       sourceMap: true,
+//       minify: true,
+//     },
+//     description: 'Get products list Lambda',
+//     environment: {
+//       NODE_OPTIONS: '--enable-source-maps',
+//       PRODUCT_AWS_REGION: 'eu-west-1',
+//     },
+//     logRetention: cdk.aws_logs.RetentionDays.ONE_DAY,
+//     // Default value
+//     memorySize: 128,
+//     // Default value
+//     timeout: cdk.Duration.seconds(3),
+// });
+const getProductsListLambdaFunction = new lambda.Function(stack, 'getProductsList', {
+    runtime: lambda.Runtime.NODEJS_20_X,
+    handler: 'index.getProductsList',
+    code: lambda.Code.fromAsset('handlers/getProductsList'),
+});
+
+const apiGW = new apigateway.LambdaRestApi(stack, 'product-service', {
+    handler: getProductsListLambdaFunction,
+    proxy: false,
+});
+
+const products = apiGW.root.addResource('products');
+products.addMethod('GET');
+
+// const product = apiGW.root.addResource('{product}');
+// products.addMethod('GET');
+
+app.synth();
+// new deployment.BucketDeployment(stack, 'DeployWebApp', {
+//     destinationBucket: bucket,
+//     sources: [deployment.Source.asset('./dist')],
+//     distribution: cloudfront,
+//     distributionPaths: ['/*'],
+// });
+
+// new cdk.CfnOutput(stack, 'Domain URL', {
+//     value: cloudfront.distributionDomainName,
+// });
---
+++ product-service/mock/mock-data.ts
@@ -0,0 +1,16 @@
+const mockGames = [
+    {
+        titile: 'Counter Strike 2',
+        count: 5000,
+        price: 1,
+        id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
+        description: 'Second version of Counter-Strike',
+    },
+    {
+        titile: 'Counter Strike 1.6',
+        count: 150000,
+        price: 100500,
+        id: '7567ec4b-b10c-48c5-9345-fc73c48a80ab',
+        description: 'Old Counter-Strike from 2000th',
+    },
+];
